2dcf2175c4708559b4a271395670009b0052feae
Philip Keller <philipjoelkeller@gmail.com>
2023-03-06 22:21:47 -0800

    Initial Commit

---
+++ Xylophone/XylophoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  XylophoneApp.swift
+//  Xylophone
+//
+//  Created by Philip Keller on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct XylophoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
