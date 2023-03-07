a711111e2f861232f8f1761ee8d19733a3c93a0c
Philip Keller <philipjoelkeller@gmail.com>
2023-03-06 22:38:55 -0800

    AppBrewery UIKit Converted to SwiftUI

--- Xylophone/ContentView.swift
+++ Xylophone/ContentView.swift
@@ -6,16 +6,72 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    
+    @State private var audioPlayer: AVAudioPlayer!
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                playSound(soundName: "C")
+            } label: {
+                Color(.red)
+                    .cornerRadius(12)
+            }
+            Button {
+                playSound(soundName: "D")
+            } label: {
+                Color(.orange)
+                    .cornerRadius(12)
+            }
+            Button {
+                playSound(soundName: "E")
+            } label: {
+                Color(.yellow)
+                    .cornerRadius(12)
+            }
+            Button {
+                playSound(soundName: "F")
+            } label: {
+                Color(.green)
+                    .cornerRadius(12)
+            }
+            Button {
+                playSound(soundName: "G")
+            } label: {
+                Color(.blue)
+                    .cornerRadius(12)
+            }
+            Button {
+                playSound(soundName: "A")
+            } label: {
+                Color(.systemIndigo)
+                    .cornerRadius(12)
+            }
+            Button {
+                playSound(soundName: "B")
+            } label: {
+                Color(.purple)
+                    .cornerRadius(12)
+            }
         }
         .padding()
+    }
+    
+    func playSound (soundName: String) {
+        guard let soundFile = NSDataAsset (name: soundName) else {
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer (data: soundFile.data)
+            audioPlayer.play ()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer.")
+        }
     }
 }
 
